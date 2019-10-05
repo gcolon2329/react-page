3619f9453a7106cec8aa6d48f134f744c7b71adb
Gabe Colon <gabriel@letca.com>
2019-10-04 20:20:01 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import NavBar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 import "./App.css";
 import Navbar from "react-bootstrap/Navbar";
@@ -29,19 +30,28 @@ class App extends React.Component {
       }
     };
   }
+
   render() {
     return (
       <Router>
         <Container className="p-0" fluid={true}>
-          <NavBar className="border-bottom">
+          <NavBar className="border-bottom" bg="transparent" expand="lg">
             <Navbar.Brand>Gabriel Colon</Navbar.Brand>
 
-            <Navbar.Toggle aria-controls="navbar-toggle">
-              <Navbar.Collapse id= "navbar-toggle">
-                <Nav>
-
-                </Nav>
-              </Navbar.Collapse>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
           </NavBar>
         </Container>
       </Router>
